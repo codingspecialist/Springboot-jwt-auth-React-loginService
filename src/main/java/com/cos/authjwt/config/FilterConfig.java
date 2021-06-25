1420d11109db3877f54f07d2d27c28972b519de1
ihongss <36065549+ihongss@users.noreply.github.com>
2021-06-25 11:07:09 +0900

    cors 필터 추가

--- src/main/java/com/cos/authjwt/config/FilterConfig.java
+++ src/main/java/com/cos/authjwt/config/FilterConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.cos.authjwt.config.filter.CorsFilter;
 import com.cos.authjwt.config.filter.jwt.JwtAuthenticationFilter;
 import com.cos.authjwt.config.filter.jwt.JwtAuthorizationFilter;
 
@@ -11,11 +12,20 @@ import com.cos.authjwt.config.filter.jwt.JwtAuthorizationFilter;
 public class FilterConfig {
 	
 	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		System.out.println("CORS 필터 등록");
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter());
+		bean.addUrlPatterns("/*");
+		bean.setOrder(0); // 낮은 번호부터 실행됨.
+		return bean;
+	}
+	
+	@Bean
 	public FilterRegistrationBean<JwtAuthenticationFilter> jwtAuthenticationFilter(){
 		System.out.println("JwtAuthenticationFilter 등록됨");
 		FilterRegistrationBean<JwtAuthenticationFilter> bean = new FilterRegistrationBean<>(new JwtAuthenticationFilter());
 		bean.addUrlPatterns("/login");
-		bean.setOrder(0); // 낮은 번호 부터 실행
+		bean.setOrder(1); // 낮은 번호 부터 실행
 		return bean;
 	}
 	
@@ -25,7 +35,7 @@ public class FilterConfig {
 		FilterRegistrationBean<JwtAuthorizationFilter> bean = new FilterRegistrationBean<>(new JwtAuthorizationFilter());
 		bean.addUrlPatterns("/post/*");
 		bean.addUrlPatterns("/user/*");
-		bean.setOrder(1); // 낮은 번호 부터 실행
+		bean.setOrder(2); // 낮은 번호 부터 실행
 		return bean;
 	}
 	
--- src/main/java/com/cos/authjwt/web/PostController.java
+++ src/main/java/com/cos/authjwt/web/PostController.java
@@ -1,5 +1,16 @@
 package com.cos.authjwt.web;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 public class PostController {
+
+	public void hello1() {
+		
+	}
+	
+	public void hello2() {
+		
+	}
 
 }
